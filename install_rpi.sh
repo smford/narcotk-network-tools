5980aad566de24aa5f696e59e189828502719876
Stephen Ford <11182275+smford@users.noreply.github.com>
2019-02-19 19:12:51 +0000

    Fix variable prefix used for bash shell interpolation

--- install_rpi.sh
+++ install_rpi.sh
@@ -9,6 +9,6 @@ cd ~/rpi-binaries
 
 for i in "${apps[@]}"
 do
-  curl -O -L https://github.com/smford/narcotk-network-tools/raw/master/binaries/rpi/%i
-  chmod +x %i
+  curl -O -L https://github.com/smford/narcotk-network-tools/raw/master/binaries/rpi/$i
+  chmod +x $i
 done
