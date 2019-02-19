ab5cb7c08503b6394130e14a992f51faab4d71f9
Stephen Ford <11182275+smford@users.noreply.github.com>
2019-02-19 18:38:05 +0000

    Moving binaries

--- compile_all.sh
+++ compile_all.sh
@@ -6,7 +6,7 @@ set -x
 
 for i in "${apps[@]}"
 do
-	GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o assets/$i-linux-x86_64 $i.go; upx assets/$i-linux-x86_64
-  GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -o assets/$i-rpi-arm5 $i.go; upx assets/$i-rpi-arm5
-  GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o assets/$i-osx-amd64 $i.go; upx assets/$i-osx-amd64
+  GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o binaries/x86_64/$i $i.go; upx binaries/x86_64/$i
+  GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -o binaries/rpi/$i $i.go; upx binaries/rpi/$i
+  GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o binaries/osx/$i $i.go; upx binaries/osx/$i
 done
