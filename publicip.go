5ce03285da7572878402500574924348c3c12de3
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-02-10 09:00:54 +0000

    Update publicip with multiple providers and AWS default

--- publicip.go
+++ publicip.go
@@ -3,13 +3,55 @@ package main
 // https://www.ipify.org
 
 import (
+	"flag"
 	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 )
 
+var (
+	ipproviderlist = map[string]string{
+		"aws":      "https://checkip.amazonaws.com",
+		"ipify":    "https://api.ipify.org",
+		"my-ip.io": "https://api.my-ip.io/ip",
+	}
+)
+
+func init() {
+	flag.String("provider", "aws", "Provider of your external IP, \"aws\", \"ipify\" or \"my-ip.io\" or \"all\", default = aws")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+}
+
 func main() {
-	res, _ := http.Get("https://api.ipify.org")
+	if strings.ToLower(viper.GetString("provider")) == "all" {
+		for k, v := range ipproviderlist {
+			fmt.Printf("%s [%s] %s\n", k, v, getIP(k))
+		}
+	} else {
+		fmt.Println(getIP(viper.GetString("provider")))
+	}
+}
+
+func getIP(ipprovider string) string {
+	ipprovider = strings.ToLower(ipprovider)
+
+	res, err := http.Get(ipproviderlist[ipprovider])
 	ip, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(ip[:len(ip)]))
+
+	returnip := string(ip[:len(ip)])
+	returnip = strings.TrimSuffix(returnip, "\n")
+
+	if err != nil {
+		fmt.Printf("Cannot discern public IP using: %s", ipprovider)
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	return returnip
 }
