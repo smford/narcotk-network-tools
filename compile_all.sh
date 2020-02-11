493307f22297f857b419910da9d6115da1eee0f8
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-02-11 14:41:48 +0000

    Renaming ER binary builds, updating compile script

--- compile_all.sh
+++ compile_all.sh
@@ -9,6 +9,6 @@ do
   GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o binaries/x86_64/$i $i.go; upx binaries/x86_64/$i
   GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -o binaries/rpi/$i $i.go; upx binaries/rpi/$i
   GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o binaries/osx/$i $i.go; upx binaries/osx/$i
-  GOOS=linux GOARCH=mipsle go build -ldflags "-s -w" -o binaries/er-x/$i $i.go; upx binaries/er-x/$i
-  GOOS=linux GOARCH=mips go build -ldflags "-s -w" -o binaries/er-4/$i $i.go; upx binaries/er-4/$i
+  GOOS=linux GOARCH=mipsle go build -ldflags "-s -w" -o binaries/er-mediatek/$i $i.go; upx binaries/er-mediatek/$i
+  GOOS=linux GOARCH=mips go build -ldflags "-s -w" -o binaries/er-cavium/$i $i.go; upx binaries/er-cavium/$i
 done
