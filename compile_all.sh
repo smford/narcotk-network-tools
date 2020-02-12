0876af902425a2bbfe339bb06ae3607d1c1cc47d
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-02-12 13:37:59 +0000

    Changing rpi arm version to 6
    cat /proc/cpuinfo
    processor	: 0
    model name	: ARMv6-compatible processor rev 7 (v6l)
    BogoMIPS	: 697.95
    Features	: half thumb fastmult vfp edsp java tls
    CPU implementer	: 0x41
    CPU architecture: 7
    CPU variant	: 0x0
    CPU part	: 0xb76
    CPU revision	: 7
    
    Hardware	: BCM2835
    Revision	: 0002
    Serial		: 0000000020180cfd

--- compile_all.sh
+++ compile_all.sh
@@ -7,7 +7,7 @@ set -x
 for i in "${apps[@]}"
 do
   GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o binaries/x86_64/$i $i.go; upx binaries/x86_64/$i
-  GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -o binaries/rpi/$i $i.go; upx binaries/rpi/$i
+  GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w" -o binaries/rpi/$i $i.go; upx binaries/rpi/$i
   GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o binaries/osx/$i $i.go; upx binaries/osx/$i
   GOOS=linux GOARCH=mipsle go build -ldflags "-s -w" -o binaries/er-mediatek/$i $i.go; upx binaries/er-mediatek/$i
   GOOS=linux GOARCH=mips go build -ldflags "-s -w" -o binaries/er-cavium/$i $i.go; upx binaries/er-cavium/$i
